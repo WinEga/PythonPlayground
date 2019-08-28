0b113c3ed530382b8bf78274592bec19c4b196a0
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-08-28 22:36:44 +0100

    ifElse programme added

---
+++ ifElse.py
@@ -0,0 +1,8 @@
+number = int(input("Enter any integer number "))
+
+if number == 10:
+    print("Entered number is equal to 10")
+elif number < 10:
+    print("Entered number is less than 10")
+else:
+    print("Entered number is grater than 10")
