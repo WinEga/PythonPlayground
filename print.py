a4b63b4c089cf8c5a87183b9a0de09b47502f908
egambarampanneerselvam <egambaram.panneerselvam@directlinegroup.co.uk>
2019-08-28 15:56:49 +0100

    Simple program to print few messages on console window and prints user values

---
+++ print.py
@@ -0,0 +1,11 @@
+print("Welcome Dear Team")
+print("Maths\n"*5)
+print('file')
+
+full_name = 'Egambaram Panneerselvam'
+age = 29
+is_new_customer = True
+
+print(full_name, age, is_new_customer)
+
+
---
+++ printUserValue.py
@@ -0,0 +1,7 @@
+name = input('What is your name? ')
+favorite_color = input('Hi ' + name + ' What is your favorite color? ')
+print(name + ' likes ' + favorite_color + ' color')
+
+weight = input('What is your weight in Kg? ')
+pound = float(weight) * 2.205
+print('Hey ' + name + ' Your weight in pound is ' + str(pound))
